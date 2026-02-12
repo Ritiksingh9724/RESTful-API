c29fbad33e6e51f2d2c3af226b92a45a48f929cb
Ritik Singh <ritiksingh9724@gmail.com>
2026-02-12 09:23:49 +0530

    Initial REST API upload

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -2,21 +2,40 @@ const { v4: uuidv4 } = require("uuid");
 
 let cart = [];
 
-exports.addToCart = (req, res) => {
-  const item = {
-    id: uuidv4(),
-    productId: req.body.productId,
-    quantity: req.body.quantity
-  };
-  cart.push(item);
-  res.status(201).json(item);
+exports.addToCart = (req, res, next) => {
+  try {
+    const item = {
+      id: uuidv4(),
+      productId: req.body.productId,
+      quantity: req.body.quantity
+    };
+
+    cart.push(item);
+    res.status(201).json(item);
+
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.viewCart = (req, res) => {
-  res.json(cart);
+exports.viewCart = (req, res, next) => {
+  try {
+    res.json(cart);
+  } catch (error) {
+    next(error);
+  }
 };
 
-exports.removeFromCart = (req, res) => {
-  cart = cart.filter(item => item.id !== req.params.id);
-  res.json({ message: "Item removed from cart" });
+exports.removeFromCart = (req, res, next) => {
+  try {
+    cart = cart.filter(item => item.id !== req.params.id);
+
+    res.json({
+      success: true,
+      message: "Item removed from cart"
+    });
+
+  } catch (error) {
+    next(error);
+  }
 };
